3d718da01c55138cba7fc00b1875378b9eb5fcf6
Sébastien Gervilla <92156546+SebastienGervilla@users.noreply.github.com>
2022-07-24 14:31:11 +0200

    Link management
    Displays links depending on the current user role

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { fetchApi } from '../utils/api-fetch';
 
 const Nav = () => {
+
+    const [userRole, setUserRole] = useState('Développeur');
+
+    useEffect(() => {
+        fetchApi('account/', 'GET', null, (data) => {
+            if (data.success === true)
+                if (JSON.stringify(data.user.role) !== JSON.stringify(userRole))
+                    setUserRole(data.user.role);
+        });
+    }, [userRole])
+
+    const manageNavLinks = () => {
+        if (userRole === 'Admin')
+            return <li><NavLink to='/app/team' >Equipe</NavLink></li>
+    }
+
     return (
         <nav>
             <ul>
                 <li><NavLink to='/app/projects' >Projets</NavLink></li>
-                <li><NavLink to='/app/team' >Equipe</NavLink></li>
+                {manageNavLinks()}
                 {/* <li><NavLink to='/' >Tickets</NavLink></li> */}
                 <li><NavLink to='/app/profile' >Profile</NavLink></li>
             </ul>
