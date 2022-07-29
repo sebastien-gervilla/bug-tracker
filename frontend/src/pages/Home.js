0ed69460a753b8fda3814bf895e2417efe0aea50
Sébastien Gervilla <92156546+SebastienGervilla@users.noreply.github.com>
2022-07-29 14:02:46 +0200

    Demo version
    Demo version added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DemoOptions from '../components/home/DemoOptions';
 import Landing from '../components/home/Landing';
 
 const Home = () => {
+
+    const [homeState, setHomeState] = useState('landing');
+
+    const manageHome = () => {
+        if (homeState === 'landing')
+            return <Landing setHomeState={setHomeState} />
+        if (homeState === 'demo')
+            return <DemoOptions />
+    };
+
     return (
         <section id='home-page'>
-            <Landing />
+            {manageHome()}
         </section>
     );
 };
